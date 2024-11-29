b672b415bc75b9be10547780a096069106f0da0c
Prateekk02 <prateek.kumar2002@gmail.com>
2024-11-29 14:43:36 +0530

    Settting up solution and urls

---
+++ browser_details/control_browser/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views as v
+
+urlpatterns = [
+    path('start/', v.start, name = "start"),
+    path('stop/', v.stop, name= "stop"),
+    path('getUrl/', v.getUrl, name= "getUrl"),
+    path('cleanUp/', v.cleanUp, name= "cleanUp")       
+]
+
+
